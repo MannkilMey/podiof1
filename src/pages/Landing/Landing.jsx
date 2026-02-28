012f878c34d92e2f585cac71578d09432c18b3c0
Jose Benitez Meyer <Jose Carlos Benitez Meyer>
2026-02-28 13:19:44 -0300

    fix: Use simple DisclaimerBanner

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useAuthStore } from '../../stores/authStore';
 import { useThemeStore } from '../../stores/themeStore';
-import DisclaimerBanner from '../../components/DisclaimerBanner';
+import DisclaimerBanner from '../../components/DIsclaimerBanner';
 
 const FONTS = `@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600;700;800;900&family=Barlow:wght@300;400;500;600&display=swap');`;
 
